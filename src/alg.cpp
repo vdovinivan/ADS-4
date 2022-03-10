78bb111725b1cb5e004d1a0763119407364198bb
vdovinivan <99171081+vdovinivan@users.noreply.github.com>
2022-03-10 14:10:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   int m = 0;
-    for (int j = 0; j < len; i++) {
+    for (int j = 0; j < len; j++) {
       if (arr[j] <= value) {
         for (int jj = 0; jj < len; jj++) {
           if (arr[j] + arr[jj] < value) {
@@ -27,9 +27,44 @@ int countPairs2(int *arr, int len, int value) {
     return r;
 }
 
+void quickSort(int* arr, int len) {
+    int a = 0;
+    int b = len - 1;
+    int c = arr[len / 2];
+    do {
+        while (arr[a] < c)
+            l++;
+        while (arr[b] > c)
+            b--;
+        if (a <= b) {
+            int t = arr[b];
+            arr[a] = arr[b];
+            arr[r] = temp;
+            a++;
+            b--;
+        }
+    } while (a <= b);
+    if (b > 0)
+        quickSort(arr, b + 1);
+    if (a < len)
+        quickSort(&arr[a], len - l);
+}
+int binSearch(int* arr, int a, int b, int value) {
+    if (b >= a) {
+        int c = a + (b - l) / 2;
+        if (arr[c] == value)
+            return binSearch(arr, a, c - 1, value)
+            + binSearch(arr, c + 1, b, value) + 1;
+        if (arr[c] > value)
+            return binSearch(arr, a, c - 1, value);
+        return binSearch(arr, c + 1, b, value);
+    }
+    return 0;
+}
+
 int countPairs3(int *arr, int len, int value) {
     int r = 0;
-    for (int j = 0; j < len - 1; i++)
+    for (int j = 0; j < len - 1; j++)
         for (int j = i + 1; j < len; j++)
             if (arr[i] + arr[j] == value) {
                 r++;
